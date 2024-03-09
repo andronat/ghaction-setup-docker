b16bbb4aff4f12bab984aeb33e78779a59eafe03
Anastasios Andronidis <anastasios.andronidis@gmail.com>
2024-03-09 03:29:56 +0100

    Reduce runDir path length

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ actionsToolkit.run(
   // main
   async () => {
     const input: context.Inputs = context.getInputs();
-    const runDir = path.join(os.homedir(), `setup-docker-action-${uuid.v4()}`);
+    const runDir = path.join(os.homedir(), `sda-${uuid.v4()}`);
 
     if (input.context == 'default') {
       throw new Error(`'default' context cannot be used.`);
